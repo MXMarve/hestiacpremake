c6cdc1ff45a4de25b71937fd192819f7d6665594
MXMarve <mxmarve86@gmail.com>
2022-02-03 19:38:51 -0600

    uploaded file lol
    ok

---
+++ web/bulk/firewall/banlist/index.php
@@ -0,0 +1,33 @@
+<?php
+
+ob_start();
+
+// Main include
+include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
+
+// Check token
+verify_csrf($_POST);
+
+// Check user
+if ($_SESSION['userContext'] != 'admin') {
+    header("Location: /list/user");
+    exit;
+}
+
+$ipchain = $_POST['ipchain'];
+$action = $_POST['action'];
+
+switch ($action) {
+    case 'delete': $cmd='v-delete-firewall-ban';
+        break;
+    default: header("Location: /list/firewall/banlist/"); exit;
+}
+
+foreach ($ipchain as $value) {
+    list($ip, $chain) = explode(":", $value);
+    $v_ip    = escapeshellarg($ip);
+    $v_chain = escapeshellarg($chain);
+    exec(HESTIA_CMD.$cmd." ".$v_ip." ".$v_chain, $output, $return_var);
+}
+
+header("Location: /list/firewall/banlist");
---
+++ web/schedule/backup/index.php
@@ -0,0 +1,25 @@
+<?php
+
+ob_start();
+include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
+
+// Check token
+verify_csrf($_GET);
+
+$v_username = escapeshellarg($user);
+exec(HESTIA_CMD."v-schedule-user-backup ".$v_username, $output, $return_var);
+if ($return_var == 0) {
+    $_SESSION['error_msg'] = _('BACKUP_SCHEDULED');
+} else {
+    $_SESSION['error_msg'] = implode('<br>', $output);
+    if (empty($_SESSION['error_msg'])) {
+        $_SESSION['error_msg'] = _('Error: Hestia did not return any output.');
+    }
+
+    if ($return_var == 4) {
+        $_SESSION['error_msg'] = _('BACKUP_EXISTS');
+    }
+}
+unset($output);
+header("Location: /list/backup/");
+exit;
